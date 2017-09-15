20ec3b19012cf9ca65389b3fb2119258f0bb2575
samithsarasan <samith.sarasan@perfomatix.com>
2017-09-15 12:53:59 +0530

    Check for multiple files

--- check-file-Existence.pl
+++ check-file-Existence.pl
@@ -1,13 +1,23 @@
 use strict;
 use warnings;
 
+$! = 1;
+
 sub main {
-    my $file = '/home/subadmin/Workspace/training/perl/download-html.pl';
-    if ( -f $file ) {
-        print("$file File found \n");
-    }
-    else {
-        print("$file File not found \n");
+
+    my @files = (
+        '/home/subadmin/Workspace/training/perl/download-html.pl',
+        '/home/subadmin/Workspace/training/perl/check-file-Existence.pl',
+        '/home/subadmin/Workspace/training/perl/home.pl',
+    );
+
+    foreach my $file (@files) {
+        if ( -f $file ) {
+            print("$file File found \n");
+        }
+        else {
+            print("$file File not found \n");
+        }
     }
 }
 
